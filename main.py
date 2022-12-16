5807d233967e52f885c72c98049bce8e0554ebd0
kurtreyn <kurtreyn@yahoo.com>
2022-12-16 04:26:15 -0500

    finish with sample code

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 
-root.geometry("500x350")
-
-
-def login():
-	print("login")
-
+root.geometry("1000x850")
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(padx=60, pady=20, fill="both", expand=True)
@@ -23,6 +18,13 @@ user_entry.pack(padx=10, pady=12)
 
 password_entry = customtkinter.CTkEntry(master=frame, placeholder_text="Username", show="*")
 password_entry.pack(padx=10, pady=12)
+
+
+def login():
+	user_input = user_entry.get()
+	print('button pressed')
+	print(user_input)
+
 
 button = customtkinter.CTkButton(master=frame, text="Login", command=login)
 button.pack(padx=10, pady=12)
